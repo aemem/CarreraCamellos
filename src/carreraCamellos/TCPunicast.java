78be4efde3f4c7af32670743ea14f3e4f462be5a
lukika <luciagorrojo@gmail.com>
2025-11-15 19:07:56 +0100

    EN TCPUnicast: Creamos los métodos para enviar, recibir y cerrar. Creamos el socket y el oos/ois. Constructor correspondiente para que cada que llegue un objeto le instanciamos el oos/ois al socket. Ya luego en enviar/recibir el "Mensaje msj"

--- src/carreraCamellos/TCPunicast.java
+++ src/carreraCamellos/TCPunicast.java
@@ -1,6 +1,35 @@
 package carreraCamellos;
 import mensajes.Mensaje;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPunicast{
 
-    // metodos para la comunicacion TCP unicast entre el servidor y UN camello
+    // metodos para la comunicacion TCP unicast entre el servidor y UN camello , el resto, METODO EVIAR Y RECIVIR
+    public Socket socket;
+    public ObjectOutputStream oos;
+    public ObjectInputStream ois;
+
+    public TCPunicast(Socket socket) throws IOException {
+        this.socket = socket;
+        this.oos = new ObjectOutputStream(socket.getOutputStream());
+        this.ois = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void enviar(Mensaje msj) throws IOException {
+        oos.writeObject(msj);
+    }
+    public Mensaje recibir() throws IOException, ClassNotFoundException {
+        Mensaje msj = (Mensaje) ois.readObject();
+        return msj;
+    }
+
+    public void cerrar() throws IOException {
+        oos.close();
+        ois.close();
+    }
 }
--- src/carreraCamellos/UDPmulticast.java
+++ src/carreraCamellos/UDPmulticast.java
@@ -2,4 +2,6 @@ package carreraCamellos;
 import mensajes.Mensaje;
 public class UDPmulticast{
     // metodos para la comunicacion entre el servidor y TODOS los camellos
+    //solo eventos de carrera
+
 }
